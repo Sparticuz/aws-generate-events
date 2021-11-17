ca55a98105d5f721b3be4addeb69f76365f0459b
Kyle <kyle@dazser.com>
2021-11-17 09:47:50 -0500

    Add s3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import s3 from "./services/s3";
 import sqs from "./services/sqs";
 import apiGateway from "./services/apigateway";
 
 export default {
   apiGateway,
+  s3,
   sqs,
 };
---
+++ src/services/s3.ts
@@ -0,0 +1,50 @@
+import type { S3Event } from "aws-lambda";
+import execute from "../spawn";
+
+export interface s3Options {
+  bucket: string;
+  key: string;
+  partition: string;
+  region: string;
+}
+
+const put = async (options?: s3Options): Promise<S3Event> => {
+  const callArguments = ["s3", "put"];
+  if (options?.bucket) {
+    callArguments.push("--bucket", options.bucket);
+  }
+  if (options?.key) {
+    callArguments.push("--key", options.key);
+  }
+  if (options?.partition) {
+    callArguments.push("--partition", options.partition);
+  }
+  if (options?.region) {
+    callArguments.push("--region", options.region);
+  }
+
+  return JSON.parse(await execute(callArguments)) as S3Event;
+};
+
+const del = async (options?: s3Options): Promise<S3Event> => {
+  const callArguments = ["s3", "delete"];
+  if (options?.bucket) {
+    callArguments.push("--bucket", options.bucket);
+  }
+  if (options?.key) {
+    callArguments.push("--key", options.key);
+  }
+  if (options?.partition) {
+    callArguments.push("--partition", options.partition);
+  }
+  if (options?.region) {
+    callArguments.push("--region", options.region);
+  }
+
+  return JSON.parse(await execute(callArguments)) as S3Event;
+};
+
+export default {
+  put,
+  delete: del
+};
